59db657eb125c14f246beb8b7c0eb6d43d63cec5
Silas Bull <BullSila402@GOTVDSB.CA>
2022-02-11 10:56:51 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace numberone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Silas";
+            string favMovie = "The Matrix";
+            Console.WriteLine($"hey {firstName.ToLower()}! i finally watched {favMovie.ToLower()} last night. i thought it was okay.");
+            favMovie = favMovie.ToUpper();
+            Console.WriteLine(favMovie);
+            Console.WriteLine(favMovie.Contains("THE"));
+            favMovie = favMovie.Replace("A", "@");
+            favMovie = favMovie.Replace("E", "3");
+            Console.WriteLine(favMovie);
+
+            string quote = "I do not control the speed at which lobsters die.";
+            Console.WriteLine(quote);
+            quote = quote.ToLower();
+            quote = quote.Replace("a", "");
+            quote = quote.Replace("e", "");
+            quote = quote.Replace("i", "");
+            quote = quote.Replace("o", "");
+            quote = quote.Replace("u", "");
+            Console.WriteLine(quote);
+
+            Console.Write("   ___     ___      ");
+            Console.WriteLine("  _    ");
+            Console.Write("  (o o)   (o o)     ");
+            Console.WriteLine(">(.)__ ");
+            Console.Write(" (  V  ) (  V  )    ");
+            Console.WriteLine(" (___/ ");
+            Console.Write("/--m-m- /--m-m-     ");
+            
+
+
+
+        }
+    }
+}
